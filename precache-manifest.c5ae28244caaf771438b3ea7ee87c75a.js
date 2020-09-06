3e3f65426800a35d8c0237b303805e537a8caca5
Roman Lykov <lykov.r.s@sberbank.ru>
2020-09-07 01:18:51 +0300

    Updates

---
+++ precache-manifest.c5ae28244caaf771438b3ea7ee87c75a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c7c1ab14371634dc8c3d44cafe5968f",
+    "url": "/rator/index.html"
+  },
+  {
+    "revision": "a2e7972dc926f81a2442",
+    "url": "/rator/static/js/2.dc022d44.chunk.js"
+  },
+  {
+    "revision": "1ee8b6858a4f156c60f3f3dcb201c371",
+    "url": "/rator/static/js/2.dc022d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "048f56365c55e0e8ff0f",
+    "url": "/rator/static/js/main.9604874e.chunk.js"
+  },
+  {
+    "revision": "c8c1401a917c0ea2d3ce",
+    "url": "/rator/static/js/runtime-main.14fb29d4.js"
+  }
+]);
--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const dateTemplate = "YYYY-MM-DD";
-export const defaultPollingTime = 1000;
-export const apiURL = "https://api.exchangeratesapi.io";
-export const defaultDateAmount = 30;
